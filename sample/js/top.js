3fa670edc094707513ad3c355c808ea1508a01e5
k_yoshida <k_yoshida@RH-1410002.local>
2018-02-05 19:02:06 +0900

    test3

--- sample/js/top.js
+++ sample/js/top.js
@@ -5,7 +5,39 @@ const medias = {audio : false, video : {
       }},
       video  = document.getElementById("video"),
       canvas = document.getElementById("canvas"),
-      ctx    = canvas.getContext("2d");
+      context    = canvas.getContext("2d");
+
+
+var fileAry = ['img/test.png'];
+
+var numFiles = fileAry.length;
+var loadedCounter = 0;
+var imgAry = [];
+
+
+
+loadImgs();
+
+
+
+function loadImgs(){
+
+    var img = new Image();
+
+    img.addEventListener('load', function(){
+        loadedCounter++;
+        imgAry.push(img);
+        if(numFiles == loadedCounter){
+            requestAnimationFrame(draw);
+        } else {
+            loadImgs();
+        }
+    }, false);
+
+    img.src = fileAry[imgAry.length];
+}
+
+
 
 navigator.getUserMedia(medias, successCallback, errorCallback);
 
@@ -22,7 +54,108 @@ function errorCallback(err) {
 function draw() {
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
-  ctx.drawImage(video, 0, 0);
-
+  context.drawImage(video, 0, 0);
+  context.drawImagee(imgAry[i], 0, 0, canvas.width, canvas.height);
   requestAnimationFrame(draw);
 }
+
+
+
+
+
+		canvasStart();
+		function canvasStart(){
+			//MouseEvent[mouseDown]
+			canvas.addEventListener("mousedown", function(e){
+				oldX = e.offsetX;
+				oldY = e.offsetY-txy;
+				canvas_mouse_event=true;
+			},false);
+			canvas.addEventListener("touchstart", function(e){
+				e.preventDefault();
+				oldX = e.changedTouches[0].pageX-offset.left;
+				oldY = e.changedTouches[0].pageY-offset.top;
+				canvas_mouse_event=true;
+			},false);
+			
+			//MouseEvent[mouseMove]
+			//canvas.addEventListener('mousemove', onMouseMove);
+    		canvas.addEventListener('touchmove', onTouchMove);
+			
+			canvas.addEventListener("mousemove", function (e){
+				if(canvas_mouse_event==true){
+					var px = e.offsetX;
+					var py =e.offsetY-txy;
+					context.strokeStyle = color;
+					context.lineWidth = bold_line;
+					context.beginPath();
+					context.lineJoin= "round";
+                    context.lineCap = "round";
+					context.moveTo(oldX, oldY);
+					context.lineTo(px, py);
+					context.closePath();
+					context.stroke();
+					oldX = px;
+					oldY = py;
+				}
+			});
+			
+			function onMouseMove(e) {
+    			e.changedTouches = [{pageX: e.offsetX+offset.left, pageY: e.offsetY+offset.top}];
+				onTouchMove(e);
+			}
+			
+			function onTouchMove(e) {
+				if(canvas_mouse_event==true){
+					var px = e.changedTouches[0].pageX-offset.left;
+					var py = e.changedTouches[0].pageY-offset.top;
+					context.strokeStyle = color;
+					context.lineWidth = bold_line;
+					context.beginPath();
+					context.lineJoin= "round";
+                    context.lineCap = "round";
+					context.moveTo(oldX, oldY);
+					context.lineTo(px, py);
+					context.closePath();
+					context.stroke();
+					oldX = px;
+					oldY = py;
+				}
+			}
+			
+			//MouseEvent[mouseUp]
+			canvas.addEventListener("mouseup", function(e){
+				canvas_mouse_event=false;
+			}, false);
+			canvas.addEventListener("touchend", function(e){
+				canvas_mouse_event=false;
+			}, false);
+			
+			//MouseEvent[mouseout]
+			canvas.addEventListener("mouseout", function(e){
+				canvas_mouse_event=false;
+			}, false);
+			
+			$("#color_area a").click(function(){
+				colorSwitch($(this).attr("id"));
+			});
+			
+			$("#bold_line").change(function(){
+				lineSwitch(this);
+			});
+			
+			$("#clear_btn").click(function(e){
+				canClear();
+				e.preventDefault();
+				return false;
+			});
+			
+			//キャンパスクリア
+			function canClear(){
+				if(confirm("キャンパスに描いたデータを消去しますか？")){
+					context.beginPath();
+					context.clearRect(0, 0, canvas.width, canvas.height);
+					//context.drawImage(image, 0, 0);
+				}
+			}
+		}
